7d9f3a36bac32019c9ee5fae35f646eaae8bac1b
Gena <genada89@gmail.com>
2026-04-10 16:13:18 +0300

    solution fix cycle

--- app/main.py
+++ app/main.py
@@ -8,16 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    for person in people:
-        Person(person["name"], person["age"])
+    persons = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife \
-                = Person.people.get(person["wife"], person["wife"])
-        elif "husband" in person and person["husband"] is not None:
+                = Person.people.get(person["wife"])
+        elif person.get("husband"):
             Person.people[person["name"]].husband \
-                = Person.people.get(person["husband"], person["husband"])
+                = Person.people.get(person["husband"])
 
     return list(Person.people.values())
