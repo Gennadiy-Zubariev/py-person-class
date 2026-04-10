cca40149055b24b79c2fb75b0a70a91cc56b9dc7
Gena <genada89@gmail.com>
2026-04-10 16:15:39 +0300

    solution fix cycle1

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    persons = [Person(person["name"], person["age"]) for person in people]
+    [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         if person.get("wife"):
